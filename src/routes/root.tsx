300d2fa244691f45bfc9aa454f7651cc7e7bc9de
Devan Pellow <devanpellow@gmail.com>
2024-03-23 15:05:44 +0100

    introduce react query client

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,11 +1,16 @@
 import Routes from "../components/routes";
 import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const RootRoute = () => {
   return (
     <div>
-      <Routes />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Routes />
+        <Outlet />
+      </QueryClientProvider>
     </div>
   );
 };
