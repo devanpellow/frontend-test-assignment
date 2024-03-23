516a4bc7d3db5ba09cc49801bd253efbf904401c
Devan Pellow <devanpellow@gmail.com>
2024-03-23 16:37:05 +0100

    add basebutton component and style basemodal

---
+++ src/components/ui/BaseButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+type ButtonProps = {
+  variant: "primary" | "secondary";
+  onClick: () => void;
+  children: React.ReactNode;
+};
+
+export const BaseButton = ({
+  variant,
+  onClick,
+  children,
+}: ButtonProps): JSX.Element => {
+  const primaryStyles = "bg-neutral-900 text-white hover:bg-black";
+  const secondaryStyles =
+    "border-2 border-gray-400 text-gray-900  hover:border-black";
+
+  const variantStyles = variant === "primary" ? primaryStyles : secondaryStyles;
+
+  return (
+    <button
+      className={`${variantStyles} py-2 px-4 rounded-full`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
--- src/components/ui/BaseModal.tsx
+++ src/components/ui/BaseModal.tsx
@@ -1,28 +1,65 @@
-import { Dialog } from "@headlessui/react";
-import { useEffect, useState } from "react";
+import { BaseButton } from "./BaseButton";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
-export const BaseModal = (): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false);
+interface BaseModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  title: string;
+  description: string;
+  confirmAction: () => void;
+  confirmText?: string;
+  cancelText?: string;
+}
 
-  useEffect(() => {
-    setIsOpen(true);
-  }, []);
+export const BaseModal = ({
+  isOpen,
+  onClose,
+  title,
+  description,
+  confirmAction,
+  confirmText = "Confirm",
+  cancelText = "Cancel",
+}: BaseModalProps): JSX.Element => {
+  const handleConfirmAction = async () => {
+    await confirmAction();
+    onClose();
+  };
 
   return (
-    <Dialog
-      open={isOpen}
-      onClose={() => setIsOpen(false)}
-      className="relative z-50 bg-red-500"
+    <Transition
+      show={isOpen}
+      enter="transition duration-100 ease-out"
+      enterFrom="transform scale-95 opacity-0"
+      enterTo="transform scale-100 opacity-100"
+      leave="transition duration-75 ease-out"
+      leaveFrom="transform scale-100 opacity-100"
+      leaveTo="transform scale-95 opacity-0"
+      as={Fragment}
     >
-      <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+      <Dialog onClose={onClose} className="fixed inset-0 z-10 overflow-y-auto">
+        <div className="fixed inset-0 bg-black/5" aria-hidden="true" />
 
-      <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-        <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-          <Dialog.Title>Title</Dialog.Title>
+        <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
+          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white p-8 text-center">
+            <Dialog.Title className='text-xl font-bold'>{title}</Dialog.Title>
 
-          <Dialog.Description>Description</Dialog.Description>
-        </Dialog.Panel>
-      </div>
-    </Dialog>
+            <Dialog.Description className='py-6'>{description}</Dialog.Description>
+
+            <div className="w-full flex justify-around">
+              <BaseButton variant="secondary" onClick={() => onClose()}>
+                {cancelText}
+              </BaseButton>
+              <BaseButton
+                variant="primary"
+                onClick={() => handleConfirmAction()}
+              >
+                {confirmText}
+              </BaseButton>
+            </div>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
